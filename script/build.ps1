4b86554a0354d78efeb40e551eaccaac0fecd1d1
crupest <crupest@outlook.com>
2018-09-25 13:08:40 +0800

    Change the structure of project.

--- script/build.ps1
+++ script/build.ps1
@@ -1,8 +1,12 @@
-vcpkg.exe install folly:x64-windows fmt:x64-windows
+function CheckLastExitCode {
+    if ($LASTEXITCODE -ne 0)
+    {
+        exit $LASTEXITCODE
+    }
+}
 
-cd ..\CruUI
-Remove-Item build -Recurse -ErrorAction Ignore
-New-Item build -ItemType Directory
-cd .\build
-cmake -G "Visual Studio 15 2017 Win64" ..
-cmake --build .
+vcpkg.exe install folly:x64-windows fmt:x64-windows
+& CheckLastExitCode
+
+& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" CruUI.sln /target:Rebuild /p:Platform=x64 /p:Configuration=Debug
+& CheckLastExitCode
