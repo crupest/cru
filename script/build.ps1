03d078796ad31eb55d70ad0aa61254b301e989a6
crupest <crupest@outlook.com>
2018-09-25 13:26:20 +0800

    ...

--- script/build.ps1
+++ script/build.ps1
@@ -8,5 +8,8 @@ function CheckLastExitCode {
 vcpkg.exe install folly:x64-windows fmt:x64-windows
 & CheckLastExitCode
 
+vcpkg.exe integrate install
+& CheckLastExitCode
+
 & "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" CruUI.sln /target:Rebuild /p:Platform=x64 /p:Configuration=Debug
 & CheckLastExitCode
