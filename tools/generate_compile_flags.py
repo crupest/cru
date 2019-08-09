9eed31954c14f2d60c906adb5b49b58fbee4ff7f
杨宇千 <crupest@outlook.com>
2019-08-10 00:17:54 +0800

    Move to clangd.

--- tools/generate_compile_flags.py
+++ tools/generate_compile_flags.py
@@ -4,17 +4,17 @@ import os.path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', action='store_true',
-                    default='Debug', help='specify build configuration')
+                    default=True, help='indicates whether is in debug mode')
 args = parser.parse_args()
 
-ccls_file_name = '.ccls'
+file_name = 'compile_flags.txt'
 
 # project root dir
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 os.chdir(project_root)
 
 global_std_standard = 'c++17'  # change this to change standard
-global_std_standard_arg = '%cpp --std={}'.format(global_std_standard)
+global_std_standard_arg = '--std={}'.format(global_std_standard)
 
 global_include_paths = [
     os.path.abspath('include')
@@ -26,29 +26,29 @@ if args.debug:
     global_definitions.append('_DEBUG')
 
 
-def generate_ccls_content(additional_include_paths=[], additional_definitions=[]):
+def generate_content(additional_include_paths=[], additional_definitions=[]):
     include_args = [
         '-I{}'.format(path) for path in additional_include_paths + global_include_paths]
     definition_args = [
         '-D{}'.format(definition) for definition in additional_definitions + global_definitions]
-    args = ['clang', global_std_standard_arg] + include_args + definition_args
+    args = [global_std_standard_arg] + include_args + definition_args
     return '\n'.join(args)
 
 
-def generate_root_ccls():
-    with open(ccls_file_name, 'w') as f:
-        print(generate_ccls_content(), file=f)
+def generate_root_file():
+    with open(file_name, 'w') as f:
+        print(generate_content(), file=f)
 
 
-def generate_win_ccls_content():
-    return generate_ccls_content(additional_definitions=['UNICODE', '_UNICODE', 'WIN32', '_WINDOWS'])
+def generate_win_content():
+    return generate_content(additional_definitions=['UNICODE', '_UNICODE', 'WIN32', '_WINDOWS'])
 
 
-def generate_win_ccls():
+def generate_win_file():
     for path in ['src/win/', 'include/cru/win/']:
-        with open(os.path.join(path, ccls_file_name), 'w') as f:
-            print(generate_win_ccls_content(), file=f)
+        with open(os.path.join(path, file_name), 'w') as f:
+            print(generate_win_content(), file=f)
 
 
-generate_root_ccls()
-generate_win_ccls()
+generate_root_file()
+generate_win_file()
