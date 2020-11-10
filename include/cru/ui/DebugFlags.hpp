bde7bdeec01574587565f782c48a18338ed66705
crupest <crupest@outlook.com>
2020-11-10 15:52:56 +0800

    ...

--- include/cru/ui/DebugFlags.hpp
+++ include/cru/ui/DebugFlags.hpp
@@ -4,5 +4,5 @@ namespace cru::ui::debug_flags {
 constexpr bool routed_event = false;
 constexpr bool layout = false;
 constexpr bool shortcut = false;
-constexpr bool text_service = true;
+constexpr bool text_service = false;
 }  // namespace cru::ui::debug_flags
