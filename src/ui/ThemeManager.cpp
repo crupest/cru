358bfe253232aa4c435cb6724d49a47aefdedda5
crupest <crupest@outlook.com>
2021-05-10 22:32:07 +0800

    ..

--- src/ui/ThemeManager.cpp
+++ src/ui/ThemeManager.cpp
@@ -17,22 +17,22 @@ ThemeManager::ThemeManager() { Init(); }
 void ThemeManager::Init() {
   theme_tree_.put("scrollbar.collapse-thumb.color",
                   colors::gray.WithAlpha(128).ToUtf8String());
-  theme_tree_.put("scrollbar.arrow.normal.color", "black");
-  theme_tree_.put("scrollbar.arrow.hover.color", "black");
-  theme_tree_.put("scrollbar.arrow.press.color", "black");
-  theme_tree_.put("scrollbar.arrow.disable.color", "black");
-  theme_tree_.put("scrollbar.arrow-background.normal.color", "seashell");
-  theme_tree_.put("scrollbar.arrow-background.hover.color", "seashell");
-  theme_tree_.put("scrollbar.arrow-background.press.color", "seashell");
-  theme_tree_.put("scrollbar.arrow-background.disable.color", "seashell");
-  theme_tree_.put("scrollbar.slot.normal.color", "seashell");
-  theme_tree_.put("scrollbar.slot.hover.color", "seashell");
-  theme_tree_.put("scrollbar.slot.press.color", "seashell");
-  theme_tree_.put("scrollbar.slot.disable.color", "seashell");
-  theme_tree_.put("scrollbar.thumb.normal.color", "gray");
-  theme_tree_.put("scrollbar.thumb.hover.color", "gray");
-  theme_tree_.put("scrollbar.thumb.press.color", "gray");
-  theme_tree_.put("scrollbar.thumb.disable.color", "gray");
+  theme_tree_.put("scrollbar.arrow.normal.color", "#505050");
+  theme_tree_.put("scrollbar.arrow.hover.color", "#505050");
+  theme_tree_.put("scrollbar.arrow.press.color", "#ffffff");
+  theme_tree_.put("scrollbar.arrow.disable.color", "#a3a3a3");
+  theme_tree_.put("scrollbar.arrow-background.normal.color", "#f1f1f1");
+  theme_tree_.put("scrollbar.arrow-background.hover.color", "#d2d2d2");
+  theme_tree_.put("scrollbar.arrow-background.press.color", "#787878");
+  theme_tree_.put("scrollbar.arrow-background.disable.color", "#f1f1f1");
+  theme_tree_.put("scrollbar.slot.normal.color", "#f1f1f1");
+  theme_tree_.put("scrollbar.slot.hover.color", "#f1f1f1");
+  theme_tree_.put("scrollbar.slot.press.color", "#f1f1f1");
+  theme_tree_.put("scrollbar.slot.disable.color", "#f1f1f1");
+  theme_tree_.put("scrollbar.thumb.normal.color", "#c1c1c1");
+  theme_tree_.put("scrollbar.thumb.hover.color", "#a8a8a8");
+  theme_tree_.put("scrollbar.thumb.press.color", "#787878");
+  theme_tree_.put("scrollbar.thumb.disable.color", "#c1c1c1");
 }
 
 gsl::not_null<std::shared_ptr<platform::graphics::IBrush>>
@@ -43,7 +43,7 @@ ThemeManager::GetBrush(std::u16string key) {
     return cached_brush_iter->second;
   }
 
-  auto color_string = ToUtf16(theme_tree_.get_value<std::string>(ToUtf8(key)));
+  auto color_string = ToUtf16(theme_tree_.get<std::string>(ToUtf8(key)));
   auto color = Color::Parse(color_string);
   if (!color) throw BadThemeResourceException("Value is not a valid color.");
   std::shared_ptr<platform::graphics::IBrush> brush =
