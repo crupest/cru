ad287bbe59b0b01cc56b90076b02a8cd9baa712e
crupest <crupest@outlook.com>
2021-08-10 01:16:37 +0800

    ..

---
+++ tools/Use-VC.ps1
@@ -0,0 +1,26 @@
+function Use-VC {
+    param(
+        [Parameter()]
+        [ValidateSet('x64', 'x86')]
+        $Arch = 'x64'
+    )
+
+    if ($Arch -eq 'x86') {
+        $p = 'x86';
+    }
+    else {
+        $p = 'amd64'
+    }
+
+    $vs=$(vswhere.exe  -format value -property installationPath)
+
+    cmd /c "$vs\VC\Auxiliary\Build\vcvars64.bat $p & set" |
+    ForEach-Object {
+        if ($_ -match '=') {
+            $v = $_ -split '='
+            Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
+        }
+    }
+    Pop-Location
+    Write-Host "Visual Studio 2019 Command Prompt variables set." -ForegroundColor Yellow
+}
