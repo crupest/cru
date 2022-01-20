6f26995173d8ad46e347afc2ebd27573cee98ce7
crupest <crupest@outlook.com>
2022-01-20 22:28:25 +0800

    ...

--- include/cru/ui/mapper/PointMapper.hpp
+++ include/cru/ui/mapper/PointMapper.hpp
@@ -1,4 +1,20 @@
 #pragma once
 #include "Mapper.hpp"
 
-namespace cru::ui::mapper {}
+namespace cru::ui::mapper {
+class CRU_UI_API PointMapper : public BasicMapper<Point> {
+ public:
+  CRU_DEFAULT_CONSTRUCTOR_DESTRUCTOR(PointMapper)
+  CRU_DELETE_COPY(PointMapper)
+  CRU_DELETE_MOVE(PointMapper)
+
+ public:
+  bool SupportMapFromString() override { return true; }
+  bool SupportMapFromXml() override { return true; }
+  bool XmlElementIsOfThisType(xml::XmlElementNode* node) override;
+
+ protected:
+  std::unique_ptr<Point> DoMapFromString(String str) override;
+  std::unique_ptr<Point> DoMapFromXml(xml::XmlElementNode* node) override;
+};
+}  // namespace cru::ui::mapper
--- src/ui/mapper/PointMapper.cpp
+++ src/ui/mapper/PointMapper.cpp
@@ -1,5 +1,27 @@
 #include "cru/ui/mapper/PointMapper.hpp"
 
 namespace cru::ui::mapper {
-  
+bool PointMapper::XmlElementIsOfThisType(xml::XmlElementNode* node) {
+  if (node->GetTag() == u"Point") {
+    return true;
+  }
+  return false;
 }
+
+std::unique_ptr<Point> PointMapper::DoMapFromString(String str) {
+  std::vector<float> values = str.ParseToFloatList();
+  if (values.size() == 2) {
+    return std::make_unique<Point>(values[0], values[1]);
+  } else if (values.size() == 1) {
+    return std::make_unique<Point>(values[0], values[0]);
+  } else {
+    throw Exception(u"Invalid Point string.");
+  }
+}
+
+std::unique_ptr<Point> PointMapper::DoMapFromXml(xml::XmlElementNode* node) {
+  auto value_attr = node->GetOptionalAttribute(u"value");
+  if (!value_attr) return std::make_unique<Point>();
+  return DoMapFromString(*value_attr);
+}
+}  // namespace cru::ui::mapper
