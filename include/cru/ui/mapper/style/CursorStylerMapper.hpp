549b072d241a8d2d1ca187bba8c1cb5190dd737e
crupest <crupest@outlook.com>
2022-01-24 18:28:25 +0800

    ...

--- include/cru/ui/mapper/style/CursorStylerMapper.hpp
+++ include/cru/ui/mapper/style/CursorStylerMapper.hpp
@@ -1,16 +1,23 @@
 #pragma once
 #include "../Mapper.hpp"
+#include "cru/ui/mapper/style/IStylerMapper.hpp"
 #include "cru/ui/style/Styler.hpp"
 
 namespace cru::ui::mapper::style {
 class CRU_UI_API CursorStylerMapper
-    : public BasicPtrMapper<ui::style::CursorStyler> {
+    : public BasicPtrMapper<ui::style::CursorStyler>,
+      public virtual IStylerMapper {
  public:
   CRU_DEFAULT_CONSTRUCTOR_DESTRUCTOR(CursorStylerMapper)
 
  public:
   bool SupportMapFromXml() override { return true; }
   bool XmlElementIsOfThisType(xml::XmlElementNode* node) override;
+
+  ClonablePtr<ui::style::Styler> MapStylerFromXml(
+      xml::XmlElementNode* node) override {
+    return MapFromXml(node);
+  }
 
  protected:
   ClonablePtr<ui::style::CursorStyler> DoMapFromXml(
