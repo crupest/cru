a6dd823a94f0e6338545a4885e9d428e83a56593
crupest <crupest@outlook.com>
2022-01-29 23:18:18 +0800

    ...

---
+++ src/theme_builder/components/StyleRuleSetEditor.cpp
@@ -0,0 +1,7 @@
+#include "StyleRuleSetEditor.hpp"
+
+namespace cru::theme_builder {
+StyleRuleSetEditor::StyleRuleSetEditor() {}
+
+StyleRuleSetEditor::~StyleRuleSetEditor() {}
+}  // namespace cru::theme_builder
