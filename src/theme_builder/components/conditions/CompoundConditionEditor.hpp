85746e9f053432bdcfb99b21997efa040eac1bc3
crupest <crupest@outlook.com>
2022-02-10 23:40:40 +0800

    ...

--- src/theme_builder/components/conditions/CompoundConditionEditor.hpp
+++ src/theme_builder/components/conditions/CompoundConditionEditor.hpp
@@ -1,1 +1,7 @@
 #pragma once
+#include "ConditionEditor.h"
+
+namespace cru::theme_builder::components::conditions {
+class CompoundConditionEditor : public ConditionEditor {};
+
+}  // namespace cru::theme_builder::components::conditions
