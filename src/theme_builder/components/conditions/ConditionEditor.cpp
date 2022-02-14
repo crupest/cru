ab80b32dc6acd7fdfd1da3b63d69535264af0ef3
crupest <crupest@outlook.com>
2022-02-14 23:03:35 +0800

    ...

--- src/theme_builder/components/conditions/ConditionEditor.cpp
+++ src/theme_builder/components/conditions/ConditionEditor.cpp
@@ -1,1 +1,11 @@
 #include "ConditionEditor.h"
+#include "cru/ui/controls/FlexLayout.h"
+
+namespace cru::theme_builder::components::conditions {
+ConditionEditor::ConditionEditor() {
+  container_.SetFlexDirection(ui::controls::FlexDirection::Vertical);
+  container_.AddChild(&label_);
+}
+
+ConditionEditor::~ConditionEditor() { container_.RemoveFromParent(); }
+}  // namespace cru::theme_builder::components::conditions
--- src/theme_builder/components/conditions/ConditionEditor.h
+++ src/theme_builder/components/conditions/ConditionEditor.h
@@ -3,7 +3,7 @@
 #include "cru/ui/controls/FlexLayout.h"
 #include "cru/ui/controls/TextBlock.h"
 
-namespace cru::theme_builder {
+namespace cru::theme_builder::components::conditions {
 class ConditionEditor : public ui::components::Component {
  public:
   ConditionEditor();
@@ -11,10 +11,15 @@ class ConditionEditor : public ui::components::Component {
   ~ConditionEditor() override;
 
  public:
-  ui::controls::Control* GetRootControl() override { return nullptr; }
+  ui::controls::Control* GetRootControl() override { return &container_; }
+
+  ui::controls::FlexLayout* GetContainer() { return &container_; }
+
+  String GetLabel() const { return label_.GetText(); }
+  void SetLabel(String label) { label_.SetText(std::move(label)); }
 
  private:
   ui::controls::FlexLayout container_;
   ui::controls::TextBlock label_;
 };
-}  // namespace cru::theme_builder
+}  // namespace cru::theme_builder::components::conditions
