65f920e9b88dc89124b286d82668ece9178a665f
crupest <crupest@outlook.com>
2022-09-29 17:49:35 +0800

    Start to work on emscripten.

--- src/platform/bootstrap/Bootstrap.cpp
+++ src/platform/bootstrap/Bootstrap.cpp
@@ -6,6 +6,8 @@
 #elif defined(CRU_PLATFORM_OSX)
 #include "cru/platform/graphics/quartz/Factory.h"
 #include "cru/platform/gui/osx/UiApplication.h"
+#elif defined(CRU_PLATFORM_EMSCRIPTEN)
+// TODO: Go fill this!
 #else
 #include "cru/platform/graphics/cairo/CairoGraphicsFactory.h"
 #endif
@@ -14,7 +16,7 @@ namespace cru::platform::bootstrap {
 cru::platform::gui::IUiApplication* CreateUiApplication() {
 #ifdef CRU_PLATFORM_WINDOWS
   return new cru::platform::gui::win::WinUiApplication();
-#elif CRU_PLATFORM_OSX
+#elif defined(CRU_PLATFORM_OSX)
   return new cru::platform::gui::osx::OsxUiApplication();
 #else
   return nullptr;
@@ -25,8 +27,10 @@ CRU_PLATFORM_BOOTSTRAP_API cru::platform::graphics::IGraphicsFactory*
 CreateGraphicsFactory() {
 #ifdef CRU_PLATFORM_WINDOWS
   return new cru::platform::graphics::direct2d::DirectGraphicsFactory();
-#elif CRU_PLATFORM_OSX
+#elif defined(CRU_PLATFORM_OSX)
   return new cru::platform::graphics::quartz::QuartzGraphicsFactory();
+#elif defined(CRU_PLATFORM_EMSCRIPTEN)
+  return nullptr; // TODO: Change this.
 #else
   return new cru::platform::graphics::cairo::CairoGraphicsFactory();
 #endif
